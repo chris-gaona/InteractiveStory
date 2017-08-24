f3c200efa77a9ef4a7c97b9615dcc6caec10fc0b
Chris Gaona <thebeloved88@sbcglobal.net>
2017-08-24 08:27:36 -0700

    Loads additional pages dynamically on button click; handles bug at end of story

--- app/src/main/java/com/chrisgaona/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/chrisgaona/interactivestory/ui/MainActivity.java
@@ -20,23 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // initialize variables attached to ui
         nameField = (EditText) findViewById(R.id.nameEditText);
         startButton = (Button) findViewById(R.id.startButton);
 
+        // on click of start button
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // get the name from the nameField
                 String name = nameField.getText().toString();
+                // call startStory function & pass in name parameter
                 startStory(name);
             }
         });
     }
 
     private void startStory(String name) {
+        // create new intent for StoryActivity
         Intent intent = new Intent(this, StoryActivity.class);
+        // access string resources from strings.xml
         Resources resources = getResources();
+        // get a specific string resource & assign it as key
         String key = resources.getString(R.string.key_name);
+        // add name as extra piece in intent
         intent.putExtra(key, name);
+        // start the new activity
         startActivity(intent);
     }
 }
--- app/src/main/java/com/chrisgaona/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/chrisgaona/interactivestory/ui/StoryActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -30,24 +31,31 @@ public class StoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        // initialize variables attached to ui
         storyImageView = (ImageView) findViewById(R.id.storyImageView);
         storyTextView = (TextView) findViewById(R.id.storyTextView);
         choice1Button = (Button) findViewById(R.id.choice1Button);
         choice2Button = (Button) findViewById(R.id.choice2Button);
 
+        // access intent
         Intent intent = getIntent();
+        // assign Key: name's value to name variable passed from MainActivity
         name = intent.getStringExtra(getString(R.string.key_name));
+        // if no name was passed in
         if (name == null || name.isEmpty()) {
+            // make name default to Friend
             name = "Friend";
         }
         Log.d(TAG, name);
 
+        // initialize a new Story in onCreate of activity
         mStory = new Story();
+        // start with page 0
         loadPage(0);
     }
 
     private void loadPage(int pageNumber) {
-        Page page = mStory.getPage(pageNumber);
+        final Page page = mStory.getPage(pageNumber);
 
         Drawable image = ContextCompat.getDrawable(this, page.getImageId());
         storyImageView.setImageDrawable(image);
@@ -57,7 +65,31 @@ public class StoryActivity extends AppCompatActivity {
         pageText = String.format(pageText, name);
         storyTextView.setText(pageText);
 
+        if (page.isFinalPage()) {
+            choice1Button.setVisibility(View.INVISIBLE);
+            choice2Button.setText(R.string.play_again_button_text);
+        } else {
+            loadButtons(page);
+        }
+    }
+
+    private void loadButtons(final Page page) {
         choice1Button.setText(page.getChoice1().getTextId());
+        choice1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice1().getNextPage();
+                loadPage(nextPage);
+            }
+        });
+
         choice2Button.setText(page.getChoice2().getTextId());
+        choice2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int nextPage = page.getChoice2().getNextPage();
+                loadPage(nextPage);
+            }
+        });
     }
 }
